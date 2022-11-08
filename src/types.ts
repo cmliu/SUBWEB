7acf222ddfd1f6c4dcd42eb1c5e007abfcfa328f
lim-kim930 <1625753207lim@gmail.com>
2022-11-08 16:25:25 +0800

    project structure adjustment

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface Options {
+    'url': string,
+    'target': string,
+    'config': string,
+    'backend': string,
+    'include': string,
+    'exclude': string,
+    'name': string,
+    'emoji': 'true' | undefined,
+    'append_type': 'true' | undefined,
+    'append_info': 'true' | undefined,
+    'scv': 'true' | undefined,
+    'udp': 'true' | undefined,
+    'list': 'true' | undefined,
+    'sort': 'true' | undefined,
+    'fdn': 'true' | undefined,
+    'insert': 'true' | undefined
+}
